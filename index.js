20faf7c39f0daeb0879a1cd7428aa982983b0c2f
Angelina Besson <formadeltiempoange@gmail.com>
2025-09-24 15:23:07 -0300

    modal en estudios

--- index.js
+++ index.js
@@ -5,13 +5,26 @@ const proyectos = document.getElementById("building-right")
 const nombre = document.getElementById("nombre")
 
 const estudios = ()=>{
-    alert("Acá van estudios")
+ document.getElementById('modal-estudios').style.display = 'block';
+
+
+document.querySelector('.close').addEventListener('click', function() {
+    document.getElementById('modal-estudios').style.display = 'none';
+});
+window.addEventListener('click', function(event) {
+    const modal = document.getElementById('modal-estudios');
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
 }
 const experiencia = ()=>{
    
 }
 const about = () => {
     document.getElementById('modal-about').style.display = 'block';
+    
 }
 
 // Agregar funcionalidad para cerrar el modal
